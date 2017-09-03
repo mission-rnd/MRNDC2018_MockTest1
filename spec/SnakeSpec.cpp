768edeef7699fa01dd3f10a1ec6baddfca98be0c
Abhijith Ravuri <souravabhijith@gmail.com>
2017-09-03 08:58:32 +0530

    Test commit

--- spec/SnakeSpec.cpp
+++ spec/SnakeSpec.cpp
@@ -55,6 +55,24 @@ namespace spec
 			Assert::AreEqual(0, check, L"Sample test failed", 1, 2, 1, 2);
 		};
 
+		[TestMethod, Timeout(1000)]
+		void Sample_Snake2()
+		{
+			int len = 5;
+			int** expectedOutput = (int**)calloc(3, sizeof(int*));
+			for (int i = 0; i < 3; i++){
+				expectedOutput[i] = (int*)calloc(100, sizeof(int));
+			}
+			expectedOutput[0][0] = 5;
+			expectedOutput[1][0] = 4;
+			expectedOutput[2][0] = 3;
+			expectedOutput[2][1] = 2;
+			expectedOutput[1][1] = 1;
+			int** Output = initialSnakePosition(len);
+			int finalAnswer = compare(Output, expectedOutput);
+			Assert::AreEqual(1, finalAnswer, L"Sample 2 snake Case failed", 1, 2, 1, 2);
+		};
+
 		
 	};
 }
